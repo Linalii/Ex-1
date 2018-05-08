3497c5bff0c461c2917c1c817e796a80cedea19c
Linalii <36931084+Linalii@users.noreply.github.com>
2018-05-08 19:13:17 +0400

    Zadanie 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Привет мир!\nВвод первого числа:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nВвод второго числа:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n a-b="+(a-b));
+            Console.ReadKey();
+        }
+    }
+}
