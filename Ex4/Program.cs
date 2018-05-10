c99b41715d4a49ee4be3c0ddfe36115cd8cf6b92
Linalii <36931084+Linalii@users.noreply.github.com>
2018-05-11 00:43:27 +0300

    Zadanie 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Ex4
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("oha.txt");
+
+            String a, c;
+            a = reader.ReadLine();
+            Console.WriteLine(a);
+            c = reader.ReadLine();
+            double b = Convert.ToDouble(a);
+            double f = Convert.ToDouble(c);
+            double r = b - f;
+            String k = Convert.ToString(r);
+            Console.WriteLine(c);
+            Console.WriteLine("otvet= " + r);
+            FileStream res = new FileStream("test.txt", FileMode.Create); 
+            StreamWriter writer = new StreamWriter(res); 
+            writer.Write(r);
+            writer.Close();
+            Console.ReadKey();
+        }
+    }
+}
+    
+
---
+++ Ex4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Ex4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DNS")]
+[assembly: AssemblyProduct("Ex4")]
+[assembly: AssemblyCopyright("Copyright © DNS 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7c6504e9-d191-4574-b9d3-9c164bff909c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
